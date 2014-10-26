ccfba3802fa46374940615fc024805bd78f38611
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2014-10-26 20:46:51 +0100

    oooops

--- boon-extras.el
+++ boon-extras.el
@@ -100,11 +100,11 @@
 
 
 (defun boon-toggle-comment (regs)
-  "Toggle comments in the region"
-  (interactive (list (boon-spec-region "toggle comment"))
+  "Toggle comments in the regions REGS."
+  (interactive (list (boon-spec-region "toggle comment")))
   (dolist (reg regs)
     (comment-or-uncomment-region (min (car reg) (cdr reg))
-                                 (max (car reg) (cdr reg))))))
+                                 (max (car reg) (cdr reg)))))
 
 (defvar boon-flycheck-map
   (let ((pmap (make-sparse-keymap)))
