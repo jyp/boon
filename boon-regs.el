ed7d2570f863bfbdb66c4d4cbe1e7d4f1d976ab9
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2015-10-24 22:41:43 +0200

    cleanup

--- boon-regs.el
+++ boon-regs.el
@@ -3,12 +3,12 @@
 ;;; Commentary:
 
 
-;; A region list has the following form: ('region (begining . end) (begining . end) ...)
+;; A region list has the following form: ((begining . end) (end . begining) ...)
 
 ;;; Code:
 
 (defun boon-normalize-reg (reg)
-  "Normalize the region REG by making sure beginning < end."
+  "Normalize the region REG by making sure car < cdr."
   (cons (boon-reg-begin reg) (boon-reg-end reg)))
 
 (defun boon-reg-to-markers (reg)
@@ -16,19 +16,23 @@
   (cons (copy-marker (car reg)) (copy-marker (cdr reg))))
 
 (defun boon-borders (reg how-much)
-  "Given a normalized region REG, return its borders, whose size is HOW-MUCH."
-  (list (cons (cdr reg) (- (cdr reg) how-much))
-        (cons (car reg) (+ (car reg) how-much))))
+  "Given a normalized region REG, return its borders (as a region list).
+The size of the borders is HOW-MUCH."
+  ;; TODO: if the results would tourch or overlap, return the input region
+  (list (cons (boon-reg-end reg)   (- (boon-reg-end reg) how-much))
+        (cons (boon-reg-begin reg) (+ (boon-reg-begin reg) how-much))))
 
 (defun boon-reg-begin (reg)
+  "The begining of region REG."
   (min (cdr reg) (car reg)))
-  
+
 (defun boon-reg-end (reg)
+  "The end of region REG."
   (max (cdr reg) (car reg)))
-  
+
 (defun boon-content (reg)
-  "Given a normalized region REG, return its contents (crop the region by 1)."
-  (cons (+ (car reg) 1) (- (cdr reg) 1)))
+  "Given a region REG, return its contents (crop the region by 1)."
+  (cons (+ (boon-reg-begin reg) 1) (- (boon-reg-end reg) 1)))
 
 (provide 'boon-regs)
 ;;; boon-regs.el ends here
