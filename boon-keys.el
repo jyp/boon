161efefd01e852e22225a7e93c0f23f58ed937ed
Jean-Philippe Bernardy <jean-philippe.bernardy@tweag.io>
2016-08-27 20:30:47 +0200

    oops

--- boon-keys.el
+++ boon-keys.el
@@ -32,7 +32,7 @@
 (define-key boon-command-map "_" 'redo)
 (define-key boon-command-map "-" 'undo)
 (dolist (number '("0" "1" "2" "3" "4" "5" "6" "7" "8" "9"))
-  (define-key boon-command-map number 'universal-argument))
+  (define-key boon-command-map number 'digit-argument))
 
 
 (define-key boon-command-map " " 'boon-drop-mark)
