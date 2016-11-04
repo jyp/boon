12198420e4a71d1321193707b2aef5a152aea228
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2016-11-04 13:16:09 +0100

    add tests

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+emacs ?= emacs
+
 cheat.pdf: cheat-sheet.hs
 	ghc --make cheat-sheet.hs -main-is CC
 	./cheat-sheet
@@ -5,3 +7,12 @@ cheat.pdf: cheat-sheet.hs
 	xelatex qwerty.tex
 	./cheat-sheet
 	xelatex qwerty.tex
+
+test:
+	$(emacs) -batch --script boon-test.el
+
+clean:
+	rm -f *.elc
+
+.PHONY: all clean test
+
---
+++ boon-test.el
@@ -0,0 +1,22 @@
+(package-initialize t)
+(package-activate 'multiple-cursors)
+(package-activate 'dash)
+(package-activate 'expand-region)
+
+(add-to-list 'load-path default-directory)
+
+(ert-deftest boon-open-next-line-and-insert-at-buffer-end ()
+  (should (string=
+           "first-line\n\n"
+           (with-temp-buffer
+             (insert "first-line")
+             (goto-char 5)
+             (boon-open-next-line-and-insert)
+             (buffer-substring-no-properties 1 (point-max))))))
+
+(setq byte-compile-error-on-warn t)
+
+(and
+ (byte-compile-file "boon.el")
+ (require 'boon)
+ (ert-run-tests-batch-and-exit))
