6a1a0ce2dca2bde4662a2d040f093bbc385de391
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2021-02-03 20:54:16 +0100

    dependencies

--- boon-pkg.el
+++ boon-pkg.el
@@ -1,10 +1,11 @@
 (define-package
   "boon"
-  "0.4"
+  "1.1.1"
   "Ergonomic Command Mode for Emacs."
   '((emacs "25.1")
-    (expand-region "0.10.0")
     (dash "2.12.0")
+    (expand-region "0.10.0")
     (multiple-cursors "1.3.0"))
+    (pcre2el "1.8")
   )
 
