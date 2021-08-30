d31550b3336d706b57df0e43bedf3e95a615ce0d
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2021-08-30 09:49:29 +0200

    pkg info update

--- boon-pkg.el
+++ boon-pkg.el
@@ -1,11 +1,10 @@
 (define-package
   "boon"
-  "1.1.1"
+  "1.2.0"
   "Ergonomic Command Mode for Emacs."
-  '((emacs "25.1")
+  '((emacs "26.1")
     (dash "2.12.0")
     (expand-region "0.10.0")
-    (multiple-cursors "1.3.0")
-    (pcre2el "1.8"))
+    (multiple-cursors "1.3.0"))
   )
 
