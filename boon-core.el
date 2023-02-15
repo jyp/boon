c5b473be96fcafc7a809ac3e77433b08d6bce016
Thomas Tuegel <ttuegel@mailbox.org>
2023-02-15 12:18:00 -0600

    boon-reset-state-for-switchw: Accept a frame as the argument
    `window-selection-change-functions` may be called with a frame instead of a
    window when the selected frame has changed. In particular, this happens at
    startup when the initial frame is created. When compiled natively, the type
    mismatch (`framep` instead of `windowp`) may cause a segmentation fault.

--- boon-core.el
+++ boon-core.el
@@ -376,9 +376,11 @@ the buffer changes."
 ;; another window), return the buffer hosting it to its "natural"
 ;; state (otherwise it's surprising to the user when coming back to it)
 (add-hook 'window-selection-change-functions
-          (defun boon-reset-state-for-switchw (window)
+          (defun boon-reset-state-for-switchw (frame-or-window)
             "Reset the boon state to natural when switching windows."
-            (let* ((old (old-selected-window))
+            (let* ((old (if (framep frame-or-window)
+                            (frame-old-selected-window (old-selected-frame))
+                          (old-selected-window)))
                    (prev-buf (window-buffer old)))
               (with-current-buffer prev-buf
                 (boon-set-natural-state)))))
