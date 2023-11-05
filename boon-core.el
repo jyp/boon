ffeb514cfe95a86254ea65677275f7d5dee6198a
Thomas Tuegel <ttuegel@mailbox.org>
2023-11-05 08:28:37 -0600

    Define boon-reset-state-for-switchw at top level
    Defining this function at top level makes it easier to attach the debugger or
    override the definiton.

--- boon-core.el
+++ boon-core.el
@@ -368,16 +368,17 @@ the buffer changes."
                   boon-toggle-character-case
                   boon-toggle-case))))
 
-;; When switching away from a window (for example by clicking in
-;; another window), return the buffer hosting it to its "natural"
-;; state (otherwise it's surprising to the user when coming back to it)
-(add-hook 'window-selection-change-functions
-          (defun boon-reset-state-for-switchw (window)
-            "Reset the boon state to natural when switching windows."
-            (let* ((old (old-selected-window))
-                   (prev-buf (window-buffer old)))
-              (with-current-buffer prev-buf
-                (boon-set-natural-state)))))
+;; When switching away from a window (for example by clicking in another
+;; window), return the buffer hosting it to its "natural" state (otherwise it's
+;; surprising to the user when coming back to it).
+(defun boon-reset-state-for-switchw (new-frame)
+  "Reset the boon state to natural when switching windows."
+  (let* ((old (old-selected-window))
+         (prev-buf (window-buffer old)))
+    (with-current-buffer prev-buf
+      (boon-set-natural-state))))
+
+(add-hook 'window-selection-change-functions #'boon-reset-state-for-switchw)
 
 (defadvice isearch-exit (after boon-isearch-set-search activate compile)
   "After isearch, highlight the search term."
